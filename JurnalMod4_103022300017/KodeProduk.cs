94e58ca3d65d11f15df16e18cbe9a6f2efd364b4
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 09:52:47 +0700

    menambahkan class KodeProduk

---
+++ JurnalMod4_103022300017/KodeProduk.cs
@@ -0,0 +1,34 @@
+﻿namespace JurnalMod4_103022300017
+{
+    class KodeProduk
+    {
+        public static string getKodeProduk(string kelurahan)
+        {
+            switch (kelurahan.ToLower())
+            {
+                case "laptop":
+                    return "E100";
+                case "smartphone":
+                    return "E101";
+                case "tablet":
+                    return "E102";
+                case "headset":
+                    return "E103";
+                case "keyboard":
+                    return "E104";
+                case "mouse":
+                    return "E105";
+                case "printer":
+                    return "E106";
+                case "monitor":
+                    return "E107";
+                case "smartwatch":
+                    return "E108";
+                case "kamera":
+                    return "E109";
+                default:
+                    return "Kode Produk Tidak Ditemukan";
+            }
+        }
+    }
+}
--- JurnalMod4_103022300017/Program.cs
+++ JurnalMod4_103022300017/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JurnalMod4_103022300017;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine(KodeProduk.getKodeProduk("Laptop"));
+    }
+}
